ef19e81974d4b530a692631ac0075ff8ebbaa90c
Hang Vang <HXVang001@mail.fresnostate.edu>
2012-09-23 02:04:16 -0700

    basic machine type and technician form
    basic machine type and technician form

---
+++ Machine Type Form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Machine Type</title>
+<meta name="Machine Type Form" content="Machine Type Form" /> 
+
+</head>
+
+<body>
+
+<h1 style="text-align:center;">Machine Type</h1>
+
+ <form action="" autocomplete="on">
+  
+ Year Made:<input type="text" name="Makename"><br>
+  <br>
+  Model:<input type="text" name="Modelname"><br>
+  <br>
+  OS:<input type="text" name="OSname"><br>
+  <br>
+  Ram:<input type="text" name="Ramname"><br>
+  <br>
+  Motherboard:<input type="text" name="Mthrbrdname"><br>
+  </form>
+
+</body>
+
+</html> 
---
+++ Technician Page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Technician Page</title>
+<meta name="Technician Page Form" content="Technician Page Form" /> 
+
+</head>
+
+<body>
+
+<h1 style="text-align:center;">Technician Page</h1>
+
+ <form action="" autocomplete="on">
+  
+ First Name:<input type="text" name="Makename"><br>
+  <br>
+ Last Name:<input type="text" name="Modelname"><br>
+  <br>
+ Phone#:<input type="text" name="OSname"><br>
+  <br>
+ Address:<input type="text" name="Ramname"><br>
+  <br>
+ Level:<input type="text" name="Mthrbrdname"><br>
+  </form>
+
+</body>
+
+</html> 
